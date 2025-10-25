a4e6725ecba113df002b7de67f9a5e2c92bc1d07
laharimorampudi22 <2320030306@klh.edu.in>
2025-10-25 10:12:44 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,20 @@
 
+async function uploadImage() {
+  const imageInput = document.getElementById("imageUpload");
+  const file = imageInput.files[0];
+  if (!file) return alert("Please upload an image first!");
+
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const response = await fetch("/predict", {
+    method: "POST",
+    body: formData,
+  });
+
+  const data = await response.json();
+  document.getElementById("result").innerHTML = `
+    <h2>Prediction: ${data.prediction}</h2>
+    <p>Care Suggestion: ${data.advice}</p>
+  `;
+}
